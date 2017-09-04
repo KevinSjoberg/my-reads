433febd1f59db7cfff76f906c3548f9d39f07a4c
Kevin Sjöberg <kev.sjoberg@gmail.com>
2017-09-04 02:17:54 +0200

    Add basic error handling

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -6,26 +6,32 @@ import { search } from './BookAPI';
 export default class SearchBooks extends Component {
   state = {
     books: [],
+    error: '',
     query: ''
   }
 
   searchBooks(query) {
-    this.setState({ query });
+    this.setState({ query, error: '' });
 
     if(query.length < 1) {
+      this.setState({ books: [] });
       return;
     }
 
     search(query, 20).then(books => {
       if (Array.isArray(books)) {
         this.setState({ books })
+      } else {
+        this.setState({ error: books.error })
       }
     });
   }
 
   render() {
-    const { books, query } = this.state;
+    const { books, error, query } = this.state;
     const { shelves, onUpdateBook } = this.props;
+
+    console.log("error", this.state.error);
 
     return (
       <div className="search-books">
@@ -40,7 +46,11 @@ export default class SearchBooks extends Component {
           </div>
         </div>
         <div className="search-books-results">
-          <BookList books={books} shelves={shelves} onUpdateBook={onUpdateBook} />
+          {error !== '' ? (
+            <h2>No books found. Error: <pre>{error}</pre></h2>
+          ) : (
+            <BookList books={books} shelves={shelves} onUpdateBook={onUpdateBook} />
+          )}
         </div>
       </div>
     );
